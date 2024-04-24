bfcc163b4383cc3c5099cb765d7727670814c05b
MaximeAverty <maxime.averty.pro@gmail.com>
2024-04-25 01:18:57 +0200

    first test of importing the words and destructuring

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+const allWords = wordsList.words
+const wordContainer = document.querySelector(".words-container")
 
+allWords.forEach(word => {
+    word = word.toLowerCase()
+    const wordDiv = document.createElement("div")
+    wordDiv.classList.add("word")
+    wordContainer.appendChild(wordDiv)
+    const letters = [...word]
+    letters.forEach(letter => {
+        const customLetter = document.createElement("letter")
+        customLetter.classList.add("letter")
+        customLetter.textContent = letter
+        wordDiv.appendChild(customLetter)
+    })
+})
