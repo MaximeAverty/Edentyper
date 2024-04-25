5de782302c6d1ef7d54d60bbbf08dc9ceb21df1e
MaximeAverty <maxime.averty.pro@gmail.com>
2024-04-25 02:00:58 +0200

    fetch random word with api and insert them in the dom

--- src/app.js
+++ src/app.js
@@ -1,16 +1,43 @@
-const allWords = wordsList.words
-const wordContainer = document.querySelector(".words-container")
 
-allWords.forEach(word => {
-    word = word.toLowerCase()
-    const wordDiv = document.createElement("div")
-    wordDiv.classList.add("word")
-    wordContainer.appendChild(wordDiv)
-    const letters = [...word]
-    letters.forEach(letter => {
-        const customLetter = document.createElement("letter")
-        customLetter.classList.add("letter")
-        customLetter.textContent = letter
-        wordDiv.appendChild(customLetter)
-    })
-})
+const wordsHandler = {
+
+    wordsList: [],
+    options: {
+        wordsNumber : 50,
+    },
+
+    getRandomWords: async () => {
+        for(let i = 0; i < wordsHandler.options.wordsNumber; i++) {
+            try{
+                const response = await fetch("https://random-word-api.herokuapp.com/word")
+                if(!response.ok) {
+                    throw new Error("Failed to fetch data")
+                }
+                const data = await response.json()
+                wordsHandler.wordsList.push(data[0])
+            } catch(e) {
+                console.error(e)
+            }
+        }
+        wordsHandler.createWords()
+    },
+
+    createWords: () => {
+        const wordContainer = document.querySelector(".words-container")
+        wordsHandler.wordsList.forEach(word => {
+            word = word.toLowerCase()
+            const wordDiv = document.createElement("div")
+            wordDiv.classList.add("word")
+            wordContainer.appendChild(wordDiv)
+            const letters = [...word]
+            letters.forEach(letter => {
+                const customLetter = document.createElement("letter")
+                customLetter.classList.add("letter")
+                customLetter.textContent = letter
+                wordDiv.appendChild(customLetter)
+            })
+        })
+    }
+
+}
+
