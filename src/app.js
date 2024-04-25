c249dc813f88d379e4d95501bec0e3c73013ab70
MaximeAverty <maxime.averty.pro@gmail.com>
2024-04-26 01:18:02 +0200

    handle change number of words

--- src/app.js
+++ src/app.js
@@ -1,17 +1,28 @@
 
 const wordsHandler = {
 
+    domElements: {
+        wordContainer: document.getElementById("wordsContainer"),
+        getAllOptionsNumber: document.querySelectorAll(".options__number"),
+        wordInput: document.getElementById("wordInput"),
+        getAllWords: null
+        
+    },
     wordsList: localWords.words,
     choosedWords: [],
     options: {
-        wordsNumber : 25,
+        wordsNumber : 50,
     },
 
     init: () => {
         wordsHandler.getRandomWords()
+
+        wordsHandler.changeWordsNumber()
+        wordsHandler.handleInput()
     },
 
     getRandomWords: () => {
+        wordsHandler.choosedWords = []
         for(let i = 0; i < wordsHandler.options.wordsNumber; i++) {
             const randomWord = Math.floor(Math.random() * wordsHandler.wordsList.length)
             wordsHandler.choosedWords.push(wordsHandler.wordsList[randomWord])
@@ -20,12 +31,11 @@ const wordsHandler = {
     },
 
     createWords: () => {
-        const wordContainer = document.querySelector(".words-container")
         wordsHandler.choosedWords.forEach(word => {
             word = word.toLowerCase()
             const wordDiv = document.createElement("div")
             wordDiv.classList.add("word")
-            wordContainer.appendChild(wordDiv)
+            wordsHandler.domElements.wordContainer.appendChild(wordDiv)
             const letters = [...word]
             letters.forEach(letter => {
                 const customLetter = document.createElement("span")
@@ -33,7 +43,33 @@ const wordsHandler = {
                 customLetter.textContent = letter
                 wordDiv.appendChild(customLetter)
             })
+            wordsHandler.domElements.getAllWords = document.querySelectorAll(".word")
         })
+    },
+
+    changeWordsNumber: () => {
+        wordsHandler.domElements.getAllOptionsNumber.forEach(opt => {
+            opt.addEventListener("click", () => {
+                wordsHandler.domElements.getAllOptionsNumber.forEach(btn => { 
+                    btn.classList.remove("option__number--active")
+                })
+                opt.classList.add("option__number--active")
+                wordsHandler.options.wordsNumber = Number(opt.textContent)
+                wordsHandler.domElements.getAllWords.forEach(word => {
+                    word.remove()
+                })
+                wordsHandler.getRandomWords()
+            })
+        })
+    },
+
+    handleInput: () => {
+
+        wordsHandler.domElements.wordInput.addEventListener("input", () => {
+            console.log(wordInput.value)
+        })
+
+
     }
 
 }
