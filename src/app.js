1f4468c401fab5dd0d3a9312ccb5b2be2c02432a
MaximeAverty <maxime.averty.pro@gmail.com>
2024-04-26 03:04:55 +0200

    First test of the keyboard typing

--- src/app.js
+++ src/app.js
@@ -5,9 +5,12 @@ const wordsHandler = {
         wordContainer: document.getElementById("wordsContainer"),
         getAllOptionsNumber: document.querySelectorAll(".options__number"),
         wordInput: document.getElementById("wordInput"),
-        getAllWords: null
+        getAllWords: null,
+        getAllLetters: null,
         
     },
+    wordIndex: 0,
+    letterIndex: 0,
     wordsList: localWords.words,
     choosedWords: [],
     options: {
@@ -19,6 +22,11 @@ const wordsHandler = {
 
         wordsHandler.changeWordsNumber()
         wordsHandler.handleInput()
+
+        wordsHandler.domElements.wordContainer.addEventListener("click", () => {
+            wordsHandler.domElements.wordInput.focus()
+        })
+
     },
 
     getRandomWords: () => {
@@ -44,6 +52,7 @@ const wordsHandler = {
                 wordDiv.appendChild(customLetter)
             })
             wordsHandler.domElements.getAllWords = document.querySelectorAll(".word")
+            wordsHandler.domElements.getAllLetters = document.querySelectorAll(".letter")
         })
     },
 
@@ -64,9 +73,24 @@ const wordsHandler = {
     },
 
     handleInput: () => {
+        
 
-        wordsHandler.domElements.wordInput.addEventListener("input", () => {
-            console.log(wordInput.value)
+        wordsHandler.domElements.wordInput.addEventListener("keyup", (e) => {
+            const wordIndex = wordsHandler.wordIndex;
+            const letterIndex = wordsHandler.letterIndex;
+            const currentWord = wordsHandler.domElements.getAllWords[wordIndex]
+            const currentLetter = currentWord.querySelectorAll(".letter")[letterIndex]
+            
+            console.log(currentLetter.textContent)
+
+            if(e.target.value === currentLetter.textContent) {
+                e.target.value = ""
+                currentLetter.classList.add("word--correct")
+                wordsHandler.letterIndex = letterIndex+1
+            } else {
+                e.target.value = ""
+                currentLetter.classList.add("word--incorrect")
+            }
         })
 
 
@@ -76,3 +100,26 @@ const wordsHandler = {
 
 wordsHandler.init()
 
+// J'écrit dans l'input.
+// Ca me place dans le premier mot et ca verifei que chaque charactere correspond a chaque lettre
+// SI ca correspond pas => je met la lettre en rouge Sinon => en blanc
+// 
+
+
+// const shortCut = wordsHandler.domElements.getAllWords
+//         let wordCount = 0;
+
+//         wordsHandler.domElements.wordInput.addEventListener("keyup", (e) => {
+            
+//             const lettersOfWord = shortCut[wordCount].querySelectorAll(".letter")
+//             lettersOfWord.forEach(letter => {
+//                 console.log(letter.textContent)
+//             })
+
+//         })
+
+
+// const currentWord = wordsHandler.domElements.getAllWords[wordIndex];
+//         const abcdefg =  wordsHandler.domElements.getAllLetters[letterIndex]
+
+//         console.log(abcdefg.textContent)
